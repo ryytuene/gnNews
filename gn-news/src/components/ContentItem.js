109174b1202223f07ed257de699efa9c7c0e1809
Ryyti <zaworski2007@gmail.com>
2023-03-27 13:26:10 +0200

    add content item

--- gn-news/src/components/ContentItem.js
+++ gn-news/src/components/ContentItem.js
@@ -1,5 +1,53 @@
-export const ContentItem = () => {
-  return (
-    <div>ContentItem</div>
-  )
+import { Button, Card, CardBody, CardHeader, Flex, Image, Link, Modal, ModalBody, ModalContent, ModalFooter, ModalOverlay, Text, useDisclosure } from "@chakra-ui/react";
+import { useSelector } from "react-redux";
+import { selectDisplayFormat } from "../store/slices/preferencesSlice";
+
+export const ContentItem = ({ news }) => {
+    const displayFormat = useSelector(selectDisplayFormat);
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
+    return (
+        <>
+            {displayFormat === 'grid' ?
+                <Card maxW='300px' onClick={onOpen} cursor='pointer' >
+                    <CardHeader>
+                        <Text><strong>{news.title}</strong></Text>
+                    </CardHeader>
+                    <CardBody>
+                        {news.urlToImage && <Image src={news.urlToImage} />}
+                        <Flex justifyContent='space-between' >
+                            <Text>{news.source.name}</Text>
+                            <Text>{new Date(news.publishedAt).toLocaleDateString()}</Text>
+                        </Flex>
+                    </CardBody>
+                </Card>
+                : <Card w='100%' onClick={onOpen} cursor='pointer' >
+                    <CardBody>
+                        <Text><strong>{news.title}</strong></Text>
+                        <Flex justifyContent='space-between' >
+                            <Text>{news.source.name}</Text>
+                            <Text>{new Date(news.publishedAt).toLocaleDateString()}</Text>
+                        </Flex>
+                    </CardBody>
+                </Card>
+            }
+            <Modal isOpen={isOpen} onClose={onClose} scrollBehavior='inside' >
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalBody mt={3}>
+                        {news.content ? news.content : news.title}
+                    </ModalBody>
+                    <ModalBody flexDir='column' >
+                        <Text>Autor: {news.author}</Text>
+                        <Link href={news.url} isExternal={true} >Źródło</Link>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button onClick={onClose}>
+                            Zamknij
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
+    )
 }
